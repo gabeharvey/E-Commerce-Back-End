031557866974f80b9d5066361a817eca72f3ef16
Gabe Harvey <harvey7@gmail.com>
2024-02-29 12:44:21 -0600

    Added Function to Get All Products from Database in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,20 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
-// The `/api/products` endpoint
+// The `/api/products` Endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
+// Get All Products
+router.get("/", async (req, res) => {
+  // Find All Products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll ({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
