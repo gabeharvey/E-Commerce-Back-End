a68e60d00891b47c4497c57a68d5e2e31566f1fc
Gabe Harvey <harvey7@gmail.com>
2024-02-29 17:51:10 -0600

    Added Sync Sequelize Models to Database Function in server.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// This will Sync Sequelize Models to the Database and Turn on Server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
 });
