517827d683c942fd51c1cdf9dd300079e2e1a2b4
Gabe Harvey <harvey7@gmail.com>
2024-02-29 16:17:30 -0600

    Added Find Category by ID Function to category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,20 +14,31 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// Find Category by ID Value
+router.get("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk (req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "Category Not Found." });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
