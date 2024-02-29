536c4410975ce522d6baa2c8ff9a157dd46c6b62
Gabe Harvey <harvey7@gmail.com>
2024-02-29 14:30:39 -0600

    Added Update Tag by ID Function to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -40,8 +40,18 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  // update a tag's name by its `id` value
+// This Updates a Tag Name by ID Value
+router.put("/:id", async (req, res) => {
+  try {
+    const updated = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+    !updated[0]
+      ? res.status(404).json({ message: "Tag Not Found." })
+      : res.status(200).json(updated);
+  } catch (err) {
+    res.status(500).json({ message: "Tag Not Updated." });
+  }
 });
 
 router.delete("/:id", (req, res) => {
