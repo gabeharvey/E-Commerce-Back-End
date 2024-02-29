d5bde57f6e6133dcffe2fa0b2d4e11b394006caa
Gabe Harvey <harvey7@gmail.com>
2024-02-29 14:22:58 -0600

    Added Create New Tag Function in tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,8 +30,14 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
-  // create a new tag
+// This Creates a New Tag
+router.post("/", async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json({ message: "New Tag Not Created."});
+  }
 });
 
 router.put("/:id", (req, res) => {
