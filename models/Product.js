ad0a584aa61787b5363f8450279fff8234387fb7
Gabe Harvey <harvey7@gmail.com>
2024-02-29 10:54:26 -0600

    Defined Product Columns in Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,22 +1,60 @@
-// import important parts of sequelize library
+// Import Important Parts of Sequelize Library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
+
+// Import our Database Connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// Initialize Product Model (table) by Extending Off Sequelize's Model Class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// Set Up Fields and Rules for Product model
 Product.init(
   {
-    // define columns
+    // These Define Product Columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    price: {
+      type: DataTypes.DECIMAL(9,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+      validate: {
+        isNumeric: true,
+      },
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
+
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
