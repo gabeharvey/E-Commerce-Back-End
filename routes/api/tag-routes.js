7c5857d3cc92ab1bb37b70832f0f813597f1768b
Gabe Harvey <harvey7@gmail.com>
2024-02-29 14:15:33 -0600

    Added Find Single Tag Function to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -14,9 +14,20 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// This Finds Single Tag by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "Tag Not Found" });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json({ message: "Tag Not Found."});
+  }
 });
 
 router.post("/", (req, res) => {
